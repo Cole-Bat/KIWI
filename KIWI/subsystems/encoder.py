730ae58e950d9f0f12784ef79e02280e00acb895
Cole-Bat <cole.batonyi@ucalgary.ca>
2025-07-06 23:59:27 -0600

    Update encoder.py

--- KIWI/subsystems/encoder.py
+++ KIWI/subsystems/encoder.py
@@ -1,4 +1,5 @@
 from phoenix5.sensors import CANCoder
+from phoenix5.sensors import CANCoderConfiguration
 import subsystems.constants
 import commands2
 
@@ -7,14 +8,32 @@ class CAN_coder(commands2.SubsystemBase):
     def __init__(self):
         super().__init__()
 
-        self.cancoder = CANCoder(subsystems.constants.CANCODER_ID_WA)
+        self.cancoders = {
+            "cancoder_1": CANCoder(subsystems.constants.CANCODER_ID_WA),  
+            "cancoder_2": CANCoder(subsystems.constants.CANCODER_ID_WB),    
+            "cancoder_3": CANCoder(subsystems.constants.CANCODER_ID_WC),  
+        }
 
-        self.configure_cancoder()
+        self.configure_cancoders()
     
-    def configure_cancoder(self):
-        pass
+    def configure_cancoders(self):
+        """Configure all encoders with consistent settings"""
+        for cancoder in self.cancoders.items():
+             # Reset encoder to zero
+                CANCoderConfiguration.initializationStrategy()
+        
 
-    def get_velocity(self):
-           velocity = self.cancoder.getVelocity()
-           velocity_rpm = velocity * 60 / 360
-           return velocity_rpm
+    def get_velocity(self, cancoder_name):
+        """Get velocity in degrees per second from specified encoder"""
+        if cancoder_name in self.cancoders:
+            return self.cancoders[cancoder_name].getVelocity()
+        else:
+            print(f"Warning: Encoder '{cancoder_name}' not found!")
+            return 0.0
+        
+    def get_all_velocities(self):
+        """Get all encoder velocities as a dictionary"""
+        velocities = {}
+        for name, cancoder in self.cancoders.items():
+            velocities[name] = cancoder.getVelocity()
+        return velocities
