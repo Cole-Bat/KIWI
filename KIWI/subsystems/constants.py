32ad20195cf35ff961a2c41e12137f1c48063e60
Cole-Bat <cole.batonyi@ucalgary.ca>
2025-08-27 13:15:31 -0600

    Constants Edits

--- KIWI/subsystems/constants.py
+++ KIWI/subsystems/constants.py
@@ -4,11 +4,12 @@
 #ascii art of the kiwi drive and what wheel is 1 2 3 would go hard here
 
 
-        
+# CAN Constants        
 CANCODER_ID_WA = 9  #CAN ID for Wheel 1
 CANCODER_ID_WB = 10  #CAN ID for Wheel 2
 CANCODER_ID_WC = 11  #CAN ID for Wheel 3
 
+# PWM Constants
 MOTOR_A1_PWM = 4    #Wheel A Motor 1 PWM port
 MOTOR_A2_PWM = 5    #Wheel A Motor 2 PWM port
 MOTOR_B1_PWM = 6    #Wheel B Motor 1 PWM port
@@ -16,7 +17,13 @@ MOTOR_B2_PWM = 7    #Wheel B Motor 2 PWM port
 MOTOR_C1_PWM = 8    #Wheel C Motor 1 PWM port
 MOTOR_C2_PWM = 9    #Wheel C Motor 2 PWM port
 
+# Motor Controller Constants
 PWM_SPEED_MODIFIER = 1.0    # leave as 1.0, only required when not applying the curve to the system
 DEADBAND_VALUE = 0.05       # Should be greater than the deadband of the talon controller
 CURVE_BASE = 2.7            # 2.7 corresponds to a a maximum of 70 % Duty cycle
-ROTATION_CURVE = 2.7      # 2.7 corresponds to a a maximum of 70 % Duty cycle
+ROTATION_CURVE = 2.7        # 2.7 corresponds to a a maximum of 70 % Duty cycle
+
+# Constants for the PID controller
+P_GAIN =  1.0
+I_GAIN = 0.0
+D_GAIN = 0.0
---
+++ KIWI/subsystems/pid_controller.py
@@ -0,0 +1,14 @@
+'''
+motor speed value in %
+
+motor speed % to theoritical equivalent velocity conversion
+
+velcoity value - velocity encoder = error
+
+PID the error
+
+motor speed % + (not sure if it should be +) PID output
+
+motor speed sent to the plant
+
+'''
